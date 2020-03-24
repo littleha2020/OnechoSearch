15ab62c812723f54b80d7c0c828a37f0c192573a
littleha2020 <littleha@126.com>
2020-03-24 12:25:10 +0800

    0.0.2

--- main.js
+++ main.js
@@ -13,13 +13,23 @@ $('.search-button').click(function () {
         break;
         case "必应":tools_api = "https://www.bing.com/search?q=";
         break;
-        case "百度地图":tools_api = "https://api.map.baidu.com/geocoder?output=html&address=";
-        break;
         case "维基":tools_api = "https://zh.wikipedia.org/w/index.php?search=";
+        break;
+        case "Stack Overflow":tools_api = "https://stackoverflow.com/search?q=";
         break;
         case "知乎":tools_api = "https://www.zhihu.com/search?type=content&q=";
         break;
         case "哔哩哔哩":tools_api = "https://search.bilibili.com/all?keyword=";
+        break;
+        case "新浪微博":tools_api = "https://s.weibo.com/weibo?q=";
+        break;
+        case "YouTube":tools_api = "https://www.youtube.com/results?search_query=";
+        break;
+        case "百度地图":tools_api = "https://api.map.baidu.com/geocoder?output=html&address=";
+        break;
+        case "快递查询":tools_api = "https://www.kuaidi100.com/?nu=";
+        break;
+        case "网购比价":tools_api = "http://www.b1bj.com/ws.aspx?key=";
         break;
     }
     window.open(tools_api + search_text);
